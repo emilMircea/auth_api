6dbfa1e340b2c03be22b4b67277a47fd9b7ff6a0
emilMircea <emil-mircea@protonmail.com>
2018-11-22 13:02:43 +0100

    add auth controller uses the command to check if auth is successful

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,12 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+
+  def authenticate
+    auth = AuthenticateUser.call(params[:email], params[:password])
+    if auth.success?
+      render json: { auth_token: auth.result }
+    else
+      render json: { error: auth.errors }, status: :unauthorized
+    end
+  end
+end
