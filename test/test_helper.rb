6929b4e47b8d93d86a094fe2dda92ed383076eb2
emilMircea <emil-mircea@protonmail.com>
2018-11-22 18:42:23 +0100

    move login for extracting token to helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require 'rails/test_help'
 module SignInHelper
   def sign_in_as(user)
     post authenticate_url, params: { email: user.email, password: 'foobar12' }, xhr: true
+    token_hash = JSON.parse @response.body
+    @valid_token = token_hash["auth_token"]
+    return @valid_token
   end    
 end
 
