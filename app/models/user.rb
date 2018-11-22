804533ff026a7e0926488d11eb32c4c022926326
emilMircea <emil-mircea@protonmail.com>
2018-11-22 13:03:13 +0100

    add validation for attributes to be present and unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates_uniqueness_of :username
+  validates_uniqueness_of :username, :email
+  validates_presence_of :username, :email 
   has_secure_password
 end
