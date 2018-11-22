cadd1d201b1f950ade36d44ef247ef84acfbaba4
emilMircea <emil-mircea@protonmail.com>
2018-11-22 13:09:03 +0100

    add module to sign in and get auth token

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+module SignInHelper
+  def sign_in_as(user)
+    post authenticate_url, params: { email: user.email, password: 'foobar12' }, xhr: true
+  end    
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include SignInHelper
 end
